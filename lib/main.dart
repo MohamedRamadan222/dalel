d655e9c6cc62bbc231272be6aa50305eb22c4c2a
Mohammed Ramadan <khaledabozead575@gmail.com>
2024-03-22 15:24:53 +0200

    App router

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
+import 'package:dalel_ramadan/core/routes/app_router.dart';
 import 'package:dalel_ramadan/core/utils/app_assets.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
 
 void main() {
   runApp(const Dalel());
 }
- class Dalel extends StatelessWidget {
-   const Dalel({super.key});
- 
-   @override
-   Widget build(BuildContext context) {
-     return MaterialApp(
-       home: Scaffold(
-         body: Container(
-           child: Image.asset(Assets.onBoardingThree),
-         ),
-       ),
-     );
-   }
- }
+
+class Dalel extends StatelessWidget {
+  const Dalel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: router,
+    );
+  }
+}
+
 
